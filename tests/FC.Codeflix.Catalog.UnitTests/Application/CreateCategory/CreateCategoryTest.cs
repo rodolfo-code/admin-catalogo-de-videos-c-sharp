bfbc882d16531a8fa6824dac8bb03d8fcd2a1dad
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2024-02-11 22:07:52 -0300

    feat: iniciando desenolvimento de camada de use cases com TDD

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
@@ -0,0 +1,13 @@
+﻿
+using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.CreateCategory;
+public class CreateCategoryTest
+{
+    [Fact(DisplayName = nameof(CreateCategory))]
+    [Trait("Application", "CreateCategory - Use Cases")]
+    public void CreateCategory()
+    {
+
+    }
+}
