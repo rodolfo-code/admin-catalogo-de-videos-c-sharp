326451aee84bd908cc05c3005f728ae8235ac16b
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2024-03-09 23:26:13 -0300

    criando getCategory

---
+++ src/FC.Codeflix.Catalog.Application/UseCases/Category/GetCategory/GetCategoryInput.cs
@@ -0,0 +1,13 @@
+﻿
+using MediatR;
+
+namespace FC.Codeflix.Catalog.Application.UseCases.Category.GetCategory;
+public class GetCategoryInput : IRequest<GetCategoryOutput>
+{
+    public Guid Id { get; set; }
+    public GetCategoryInput(Guid id)
+    {
+        Id = id;
+    }
+
+}
