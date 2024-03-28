a9c4f7f93ab8fa23542292ea150480c5e35bed43
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2024-03-28 08:14:10 -0300

    finalizando testes e useCases GetCategory

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/GetCategory/GetCategoryInputValidatorTest.cs
@@ -0,0 +1,50 @@
+﻿using FC.Codeflix.Catalog.Application.UseCases.Category.GetCategory;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.GetCategory;
+
+[Collection(nameof(GetCategoryTestFixture))]
+public class GetCategoryInputValidatorTest
+{
+    private readonly GetCategoryTestFixture _fixture;
+
+    public GetCategoryInputValidatorTest(GetCategoryTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(ValidationOK))]
+    [Trait("Application", "GetCategoryInputValidation - UseCases")]
+    public void ValidationOK()
+    {
+        var validInput = new GetCategoryInput(Guid.NewGuid());
+        var validator = new GetCategoryInputValidator();
+
+        var validationResult = validator.Validate(validInput);
+
+        validationResult.Should().NotBeNull();
+        validationResult.IsValid.Should().BeTrue();
+        validationResult.Errors.Should().HaveCount(0);
+    }
+
+    [Fact(DisplayName = nameof(InvalidWhenEmptyGuidId))]
+    [Trait("Application", "GetCategoryInputValidation - UseCases")]
+    public void InvalidWhenEmptyGuidId()
+    {
+        var invalidIalidInput = new GetCategoryInput(Guid.Empty);
+        var validator = new GetCategoryInputValidator();
+
+        var validationResult = validator.Validate(invalidIalidInput);
+
+        validationResult.Should().NotBeNull();
+        validationResult.IsValid.Should().BeFalse();
+        validationResult.Errors.Should().HaveCount(1);
+        validationResult.Errors[0].ErrorMessage.Should().Be("'Id' deve ser informado.");
+    }
+}
